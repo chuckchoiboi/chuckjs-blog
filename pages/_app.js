c50d357449ba3327e185d7011ae6aeab0d32ad2f
Chuck Choi <hyunchoi94@gmail.com>
2023-06-20 14:22:52 -0700

    add header to the layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { useEffect, useState } from 'react';
+import { Layout } from '../components';
+
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }

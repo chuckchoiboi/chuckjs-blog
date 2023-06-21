f0e612febd2f273013d4b2cd7227af724d128251
Chuck Choi <hyunchoi94@gmail.com>
2023-06-21 09:41:37 -0700

    add post detail skeleton structure

---
+++ components/Author.jsx
@@ -0,0 +1,8 @@
+const Author = () => {
+	return (
+		<div>
+			<h1>Author</h1>
+		</div>
+	);
+};
+export default Author;
---
+++ components/CommentsForm.jsx
@@ -0,0 +1,8 @@
+const CommentsForm = () => {
+	return (
+		<div>
+			<h1>CommentsForm</h1>
+		</div>
+	);
+};
+export default CommentsForm;

75b6454cf86cf16c0612ad2c1d9fd1fd39dd00d6
Chuck Choi <hyunchoi94@gmail.com>
2023-06-22 08:56:59 -0700

    swap layout to mirror

--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,20 @@ export default function Home({ posts }) {
 				<title>ChuckJS Blog</title>
 				<link rel="stylesheet" href="/favicon.ico" />
 			</Head>
-			<FeaturedPosts />
 			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-				<div className="lg:col-span-8 col-span-1">
-					{posts.map((post) => (
-						<PostCard post={post.node} key={post.title} />
-					))}
-				</div>
 				<div className="lg:col-span-4 col-span-1">
 					<div className="lg:sticky relative top-8">
 						<PostWidget />
 						<Categories />
 					</div>
 				</div>
+				<div className="lg:col-span-8 col-span-1">
+					{posts.map((post) => (
+						<PostCard post={post.node} key={post.title} />
+					))}
+				</div>
 			</div>
+			<FeaturedPosts />
 		</div>
 	);
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -13,12 +13,6 @@ const PostDetails = ({ post }) => {
 	return (
 		<div className="container mx-auto px-10 mb-8">
 			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-				<div className="col-span-1 lg:col-span-8">
-					<PostDetail post={post} />
-					<Author author={post.author} />
-					<CommentsForm slug={post.slug} />
-					<Comments slug={post.slug} />
-				</div>
 				<div className="col-span-1 lg:col-span-4">
 					<div className="relative lg:sticky top-8">
 						<PostWidget
@@ -29,6 +23,12 @@ const PostDetails = ({ post }) => {
 						/>
 						<Categories />
 					</div>
+				</div>
+				<div className="col-span-1 lg:col-span-8">
+					<PostDetail post={post} />
+					<Author author={post.author} />
+					<CommentsForm slug={post.slug} />
+					<Comments slug={post.slug} />
 				</div>
 			</div>
 		</div>
